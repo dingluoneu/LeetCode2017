5a022c8bb50e4285ca1cb113a22825fb9760a324
dingluoneu <dingluo.bupt@gmail.com>
2017-10-03 14:11:54 -0400

    167.Two Sum II.

---
+++ src/array/TwoSumII.java
@@ -0,0 +1,22 @@
+package array;
+
+public class TwoSumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
