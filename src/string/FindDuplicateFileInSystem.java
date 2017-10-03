8e1032e6fcd957a3a1e3f1cc16f9d9318a882ee1
dingluoneu <dingluo.bupt@gmail.com>
2017-10-02 21:18:07 -0400

    609.Find Duplicate File in System, new method which works in Java7.

--- src/string/FindDuplicateFileInSystem.java
+++ src/string/FindDuplicateFileInSystem.java
@@ -1,9 +1,6 @@
 package string;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +11,33 @@ public class FindDuplicateFileInSystem {
 
         findDuplicate(paths);
 
+    }
+
+    public List<List<String>> findDuplicates(String[] paths) {
+        List<List<String>> res = new ArrayList<>();
+        if (paths == null || paths.length == 0) return res;
+
+        Map<String, Set<String>> map = new HashMap<>();
+
+        for (String path : paths) {
+            String[] strs = path.split("\\s+");
+
+            for (int i = 1; i < strs.length; i++) {
+                int index = strs[i].indexOf("(");
+                String content = strs[i].substring(index);
+                String fileName = strs[0] + "/" + strs[i].substring(0, index);
+                Set<String> fileNames = map.getOrDefault(content, new HashSet<>());
+                fileNames.add(fileName);
+                map.put(content, fileNames);
+            }
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key).size() > 1) {
+                res.add(new ArrayList<>(map.get(key)));
+            }
+        }
+        return res;
     }
 
     public static List<List<String>> findDuplicate(String[] paths) {
