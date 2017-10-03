4fb490e9c8be5c7bb590ce0e262e25646942a8e3
dingluoneu <dingluo.bupt@gmail.com>
2017-10-03 14:55:14 -0400

    189.Rotate Array.

---
+++ src/array/RotateArray.java
@@ -0,0 +1,28 @@
+package array;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+
+    }
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
